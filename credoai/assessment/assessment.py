661e1df3ebe6d7bacbdd4a3e0394acbcecb77978
Ian Eisenberg <ian@credo.ai>
2021-12-16 18:14:18 -0800

    formatted

--- credoai/assessment/assessment.py
+++ credoai/assessment/assessment.py
@@ -4,8 +4,8 @@ Defines abstract base class for all CredoAssessments
 
 from abc import ABC, abstractmethod
 from credoai.utils.common import ValidationError
-from typing import List, Union
 import pandas as pd
+
 
 class CredoAssessment(ABC):
     """Abstract base class for all CredoAssessments
@@ -20,7 +20,8 @@ class CredoAssessment(ABC):
         Instantiation of funtionality CredoModel and/or CredoData
         must define to run this asssesment. If defined, enables
         automated validation and selection of asssessment
-    """    
+    """
+
     def __init__(self, name, module, requirements=None):
         self.name = name
         self.module = module
@@ -28,35 +29,35 @@ class CredoAssessment(ABC):
         if requirements is None:
             requirements = AssessmentRequirements()
         self.requirements = requirements
-    
+
     @abstractmethod
     def init_module(self, *, scope=None, model=None, data=None):
         """ Initializes the assessment module
-        
+
         Transforms the scope, CredoModel and CredoData into the proper form
         to create a runnable assessment.
 
         See the lens_customization notebook for examples
-        
+
         Parameters
         ------------
         scope : dict
             dictionary containing kwargs for the module defined by the scope
         model : CredoModel, optional
         data : CredoData, optional
-        
+
         Example:
         def build(self, ...):
             y_pred = CredoModel.pred_fun(CredoData.X)
             y = CredoData.y
             self.initialized_module = self.module(y_pred, y)
-        
+
         """
         pass
-            
+
     def run(self, **kwargs):
         return self.initialized_module.run(**kwargs)
-    
+
     def prepare_results(self, metadata=None, **kwargs):
         results = self.initialized_module.prepare_results(**kwargs)
         results = self._standardize_results(results)
@@ -64,9 +65,9 @@ class CredoAssessment(ABC):
         # add metadata
         metadata = metadata or {}
         metadata['assessment'] = self.name
-        results=results.assign(**metadata)
+        results = results.assign(**metadata)
         return results
-    
+
     def _standardize_results(self, results):
         if type(results) == dict:
             results = pd.Series(results, name='value').to_frame()
@@ -80,23 +81,24 @@ class CredoAssessment(ABC):
         else:
             raise TypeError("Results format not recognized")
         return results
-    
+
     def _validate_results(self, results):
-        if (type(results) != pd.DataFrame 
+        if (type(results) != pd.DataFrame
             or results.index.name != 'metric'
-            or 'value' not in results.columns):
-            raise ValidationError(f'{self.name} assessment results not in correct format')
-        
-    def check_requirements(self, 
-                           credo_model=None, 
+                or 'value' not in results.columns):
+            raise ValidationError(
+                f'{self.name} assessment results not in correct format')
+
+    def check_requirements(self,
+                           credo_model=None,
                            credo_data=None):
         """
         Defines the functionality needed by the assessment
-        
+
         Returns a list of functions that a CredoModel must
         instantiate to run. Defining this function supports
         automated assessment inference by Lens. 
-        
+
         Returns
         ----------
         credo.asseesment.AssessmentRequirements
@@ -109,12 +111,12 @@ class CredoAssessment(ABC):
 
 
 class AssessmentRequirements:
-    def __init__(self, 
+    def __init__(self,
                  model_requirements=None,
                  data_requirements=None):
         """
         Defines requirements for an assessment
-        
+
         Parameters
         ------------
         requirements : List(Union[List, str])
@@ -126,7 +128,7 @@ class AssessmentRequirements:
         """
         self.model_requirements = model_requirements or []
         self.data_requirements = data_requirements or []
-        
+
     def check_requirements(self, credo_model=None, credo_data=None):
         for artifact, requirements in \
             [(credo_model, self.model_requirements),
@@ -149,4 +151,4 @@ class AssessmentRequirements:
 
     def get_requirements(self):
         return {'model_requirements': self.model_requirements,
-                'data_requirements': self.data_requirements} 
+                'data_requirements': self.data_requirements}
