2c45d9cd5ae22fe3010fa987f037ec4ecbfef457
Ian Eisenberg <ian@credo.ai>
2021-12-22 11:00:15 -0800

    brought usable assessments to assessment

--- credoai/assessment/__init__.py
+++ credoai/assessment/__init__.py
@@ -2,3 +2,7 @@
 CredoAssessments are adapters connecting CredoModels and/or CredoData
 to Modules
 """
+
+from .assessments import list_assessments
+usable_assessments = list_assessments()
+globals().update({k: mod for k, mod in usable_assessments})
