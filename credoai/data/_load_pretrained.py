9579690c2515199403933a76e5b87262fdc6f247
amrasekh <92744575+amrasekh@users.noreply.github.com>
2022-01-27 14:53:15 -0500

    added docstring

--- credoai/data/_load_pretrained.py
+++ credoai/data/_load_pretrained.py
@@ -3,6 +3,19 @@ from credoai.data.utils import get_data_path
 from sentence_transformers import SentenceTransformer
 
 def load_lr_toxicity(verbose=True):
+    """A builtin pretrained toxicity assessment model.
+    This model is a very basic and limited, and is intended for demo purposes ONLY.
+
+    Parameters
+    ----------
+    verbose : bool, optional
+        Progress messages will be printed if True, by default True
+
+    Returns
+    -------
+    dict
+        {'model': a pretrained sklearn.linear_model.LogisticRegression model, 'encoder': a SentenceTransformer text encoder}
+    """    
     lr_model_path = get_data_path('nlp_generator_analyzer/persisted_models/lr_toxicity.joblib')
     lr_model = joblib.load(lr_model_path)
     if verbose:
