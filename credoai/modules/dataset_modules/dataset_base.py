dfa3474c8ebf74e86cea0ebc10c5a8b816b10d96
amrasekh <92744575+amrasekh@users.noreply.github.com>
2022-02-01 11:19:55 -0500

    added proxy assess in feature space

--- credoai/modules/dataset_modules/dataset_base.py
+++ credoai/modules/dataset_modules/dataset_base.py
@@ -6,7 +6,7 @@ from sklearn.pipeline import Pipeline
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import StratifiedKFold
 from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import roc_auc_score, make_scorer
+from sklearn.metrics import roc_auc_score, make_scorer, normalized_mutual_info_score
 
 import pandas as pd
 
@@ -24,8 +24,10 @@ class DatasetModule(CredoModule):
         pipe = self._make_pipe()
         cv_results = self._run_cv(pipe)
         group_differences = self._group_differences()
+        feature_space_proxy_scores = self._assess_proxy_in_feature_space()
         self.results = {'sensitive_feature_roc_auc': cv_results.mean(),
-                        'group_diffs': group_differences}  
+                        'group_diffs': group_differences,
+                        'feature_space_proxy_scores': feature_space_proxy_scores}  
         return self  
     
     def prepare_results(self):
@@ -61,4 +63,44 @@ class DatasetModule(CredoModule):
                [('scaler', StandardScaler()),
                 ('model', model)])
         return pipe
+
+    def _assess_proxy_in_feature_space(
+        self, excluded_columns=None, method="mutual_information"
+    ):
+        """Assesses features based on how much they serve as a proxy for a sensitive attribute
+        This method relies on features information only (label information is not used)
+
+        Parameters
+        ----------
+        excluded_columns : list[str]
+            List of columns to exclude from the assessment
+        method : str, optional
+            Method to use for quantifying the magnitude of proxy, by default 'mutual_information'
+            Choices:
+                'mutual_information' (the "amount of information" in normalized nats obtained about a feature by observing another feature)
+
+        Returns
+        -------
+        dict
+            Keys are feature names and values are their proxy scores
+            Score between 0.0 and 1.0
+        """
+        proxy_scores = {}
+        
+        if method == "mutual_information":
+            # Exclude features of type float
+            float_cols = list(self.X.select_dtypes(include=["float16", "float32", "float64"]))
+            if excluded_columns:
+                excluded_columns.extend(float_cols)
+            else:
+                excluded_columns = float_cols
+
+            for column in self.X.drop(excluded_columns, axis=1):
+                proxy_scores[column] = normalized_mutual_info_score(
+                    self.X[column], self.sensitive_features
+                )
+
+        return proxy_scores
+
+    
         
