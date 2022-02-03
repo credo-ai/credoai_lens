eb2737f66fd8b6950b225299cbcfb1e36ff3980b
amrasekh <92744575+amrasekh@users.noreply.github.com>
2022-02-03 13:17:35 -0500

    updated mutual information scripts

--- credoai/modules/dataset_modules/dataset_base.py
+++ credoai/modules/dataset_modules/dataset_base.py
@@ -2,11 +2,13 @@ from credoai.modules.credo_module import CredoModule
 from credoai.utils.common import NotRunError
 from credoai.utils.model_utils import get_gradient_boost_model
 from itertools import combinations
+import numpy as np
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import StratifiedKFold
 from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import roc_auc_score, make_scorer, normalized_mutual_info_score
+from sklearn.metrics import roc_auc_score, make_scorer
+from sklearn.feature_selection import mutual_info_classif, mutual_info_regression
 
 import pandas as pd
 
@@ -24,10 +26,10 @@ class DatasetModule(CredoModule):
         pipe = self._make_pipe()
         cv_results = self._run_cv(pipe)
         group_differences = self._group_differences()
-        feature_space_proxy_scores = self._assess_proxy_in_feature_space()
+        normalized_mutual_information = self._calculate_mutual_information()
         self.results = {'sensitive_feature_roc_auc': cv_results.mean(),
                         'group_diffs': group_differences,
-                        'feature_space_proxy_scores': feature_space_proxy_scores}  
+                        'normalized_mutual_information': normalized_mutual_information}  
         return self  
     
     def prepare_results(self):
@@ -64,43 +66,85 @@ class DatasetModule(CredoModule):
                 ('model', model)])
         return pipe
 
-    def _assess_proxy_in_feature_space(
-        self, excluded_columns=None, method="mutual_information"
-    ):
-        """Assesses features based on how much they serve as a proxy for a sensitive attribute
-        This method relies on features information only (label information is not used)
+    
+    def _find_categorical_features(self, df):
+        """Estimates categorical features
+        Logic: all non-float-type columns are categorical
 
         Parameters
         ----------
-        excluded_columns : list[str]
-            List of columns to exclude from the assessment
-        method : str, optional
-            Method to use for quantifying the magnitude of proxy, by default 'mutual_information'
-            Choices:
-                'mutual_information' (the "amount of information" in normalized nats obtained about a feature by observing another feature)
+        df : pandas.dataframe
+            A dataframe
+
+        Returns
+        -------
+        list
+            Names of categorical features
+        """        
+        cols = df.columns
+        float_cols = df.select_dtypes(include=[np.float]).columns
+        cat_cols = list(set(cols) - set(float_cols))
+        return cat_cols
+
+    def _calculate_mutual_information(self, categorical_features=None, normalize=True):
+        """Calculates normalized mutual information between sensitive feature and other features
+        Mutual information is the "amount of information" obtained about the sensitive feature by observing the other feature.
+        It can therefore be used to proxy detection purposes.
+
+        Parameters
+        ----------
+        categorical_features : [str], optional
+            List of the categorical features (including the sensitive attribute) in the dataset, by default None
+            If not provided, all non-float-type features are considered as categorical features
 
         Returns
         -------
         dict
-            Keys are feature names and values are their proxy scores
-            Score between 0.0 and 1.0
-        """
-        proxy_scores = {}
-        
-        if method == "mutual_information":
-            # Exclude features of type float
-            float_cols = list(self.X.select_dtypes(include=["float16", "float32", "float64"]))
-            if excluded_columns:
-                excluded_columns.extend(float_cols)
+            Normalized mutual information between sensitive feature and features and their considered feature type (categorical/continuous)
+            Normalized mutual information between sensitive feature and itself is always 1, but is included to report its considered type
+        """        
+    
+        # Create a pandas dataframe of features and sensitive feature
+        if isinstance(self.sensitive_features, pd.Series):
+            df = pd.concat([self.X, self.sensitive_features], axis=1)
+            sensitive_feature_name = self.sensitive_features.name
+        else:
+            df = self.X.copy()
+            df['sensitive_feature'] = sensitive_features
+            sensitive_feature_name = 'sensitive_feature'
+
+        # Estimate categorical features, if not provided
+        if not categorical_features:
+            categorical_features = self._find_categorical_features(df)
+
+        # Encode categorical features
+        for col in categorical_features:
+            df[col] = df[col].astype('category').cat.codes
+
+        discrete_features = [True if col in categorical_features else False for col in df.columns]
+
+        # Use the right mutual information methods based on the feature type of the sensitive attribute
+        if sensitive_feature_name in categorical_features:
+            mi = mutual_info_classif(df, df[sensitive_feature_name], discrete_features=discrete_features, random_state=42)
+        else:
+            mi = mutual_info_regression(df, df[sensitive_feature_name], discrete_features=discrete_features, random_state=42)
+
+        # Normalize the mutual information values, if requested
+        mi = pd.Series(mi, index=df.columns)
+        if normalize:
+            mi = mi/mi.max()
+
+        # Create the results
+        mi = mi.sort_index().to_dict() 
+        results = {}
+        for k, v in mi.items():
+            if k in categorical_features:
+                results[k] = {'value':v, 'feature_type': 'categorical'}
             else:
-                excluded_columns = float_cols
+                results[k] = {'value':v, 'feature_type': 'continuous'}
 
-            for column in self.X.drop(excluded_columns, axis=1):
-                proxy_scores[column] = normalized_mutual_info_score(
-                    self.X[column], self.sensitive_features
-                )
-
-        return proxy_scores
+        return results
+    
 
     
         
