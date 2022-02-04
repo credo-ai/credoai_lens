0647b27d66200f80a49f29c6f1c9b79509dd7ff3
amrasekh <92744575+amrasekh@users.noreply.github.com>
2022-02-04 14:22:53 -0500

    added docstrings and a bug fix

--- credoai/utils/nlp_utils.py
+++ credoai/utils/nlp_utils.py
@@ -5,24 +5,73 @@ from transformers import (OpenAIGPTLMHeadModel, OpenAIGPTTokenizer,
 from credoai.data import load_lr_toxicity
 
 def text_generator(prompt, tokenizer, model):
+    """Generates text for a given prompt, tokenizer, and model
+
+    Parameters
+    ----------
+    prompt : str
+        prompt text
+    tokenizer : transformers Tokenizer
+        a transformers tokenizer
+    model : transformers Model
+        a transformers model     
+    
+    Returns
+    -------
+    str
+        response text
+    """  
     inputs = tokenizer.encode(prompt, return_tensors='pt')
     outputs = model.generate(inputs, max_length=max(30, len(inputs[0])+1), do_sample=True)
     response = tokenizer.decode(outputs[0], skip_special_tokens=True)[len(prompt):]
+    return response
     
 def gpt1_text_generator(prompt):
+    """Generates text for a given prompts using GPT1 model
+
+    Parameters
+    ----------
+    prompt : str
+        prompt text
+
+    Returns
+    -------
+    str
+        response text
+    """   
     tokenizer_gpt1 = OpenAIGPTTokenizer.from_pretrained('openai-gpt')
     model_gpt1 = OpenAIGPTLMHeadModel.from_pretrained('openai-gpt', pad_token_id=tokenizer_gpt1.eos_token_id)
-    text_generator(prompt, tokenizer_gpt1, model_gpt1)
+    response = text_generator(prompt, tokenizer_gpt1, model_gpt1)
     return response
 
 def gpt2_text_generator(prompt):
+    """Generates text for a given prompts using GPT2 model
+    
+    Parameters
+    ----------
+    prompt : str
+        prompt text
+    
+    Returns
+    -------
+    str
+        response text
+    """   
     tokenizer_gpt2 = GPT2Tokenizer.from_pretrained('gpt2')
     model_gpt2 = GPT2LMHeadModel.from_pretrained('gpt2', 
                     pad_token_id=GPT2_TOKENIZER.eos_token_id)
-    text_generator(prompt, tokenizer_gpt2, model_gpt2)
+    response = text_generator(prompt, tokenizer_gpt2, model_gpt2)
     return response
 
 def get_default_nlp_assessments():
+    """Loads the default nlp assessment model
+    The model assesses a text for toxicity
+
+    Returns
+    -------
+    dict
+        prediction model (sklearn.linear_model.LogisticRegression) and sentence encoding model (SentenceTransformer)
+    """   
     loaded = load_lr_toxicity()
     lr_model = loaded['model']
     st_encoder = loaded['encoder']
