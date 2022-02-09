7807dd7eb021f966331be0bbbe218fffc86a529e
Ian Eisenberg <ian@credo.ai>
2022-02-08 23:14:05 -0800

    removed default assessment functions, as they are only good for demo purposes. Fixed issue with incredibly slow gpt1/2 performance

--- credoai/utils/nlp_utils.py
+++ credoai/utils/nlp_utils.py
@@ -4,6 +4,8 @@ from transformers import (OpenAIGPTLMHeadModel, OpenAIGPTTokenizer,
                           GPT2LMHeadModel, GPT2Tokenizer)
 from credoai.data import load_lr_toxicity
 
+GENERATOR_CACHE = {}
+
 def text_generator(prompt, tokenizer, model):
     """Generates text for a given prompt, tokenizer, and model
 
@@ -38,9 +40,17 @@ def gpt1_text_generator(prompt):
     -------
     str
         response text
-    """   
-    tokenizer_gpt1 = OpenAIGPTTokenizer.from_pretrained('openai-gpt')
-    model_gpt1 = OpenAIGPTLMHeadModel.from_pretrained('openai-gpt', pad_token_id=tokenizer_gpt1.eos_token_id)
+    """ 
+    # get or cache tokenizer
+    tokenizer_gpt1 = GENERATOR_CACHE.get('tokenizer_gpt1')
+    if tokenizer_gpt1 is None:
+        tokenizer_gpt1 = OpenAIGPTTokenizer.from_pretrained('openai-gpt')
+        GENERATOR_CACHE['tokenizer_gpt1'] = tokenizer_gpt1
+    # get or cache model
+    model_gpt1 = GENERATOR_CACHE.get('model_gpt1')
+    if model_gpt1 is None:
+        model_gpt1 = OpenAIGPTLMHeadModel.from_pretrained('openai-gpt', pad_token_id=tokenizer_gpt1.eos_token_id)
+        GENERATOR_CACHE['model_gpt1'] = model_gpt1
     response = text_generator(prompt, tokenizer_gpt1, model_gpt1)
     return response
 
@@ -57,13 +67,20 @@ def gpt2_text_generator(prompt):
     str
         response text
     """   
-    tokenizer_gpt2 = GPT2Tokenizer.from_pretrained('gpt2')
-    model_gpt2 = GPT2LMHeadModel.from_pretrained('gpt2', 
-                    pad_token_id=tokenizer_gpt2.eos_token_id)
+        # get or cache tokenizer
+    tokenizer_gpt2 = GENERATOR_CACHE.get('tokenizer_gpt2')
+    if tokenizer_gpt2 is None:
+        tokenizer_gpt2 = GPT2Tokenizer.from_pretrained('gpt2')
+        GENERATOR_CACHE['tokenizer_gpt2'] = tokenizer_gpt2
+    # get or cache model
+    model_gpt2 = GENERATOR_CACHE.get('model_gpt2')
+    if model_gpt2 is None:
+        model_gpt2 = GPT2LMHeadModel.from_pretrained('gpt2', pad_token_id=tokenizer_gpt2.eos_token_id)
+        GENERATOR_CACHE['model_gpt2'] = model_gpt2
     response = text_generator(prompt, tokenizer_gpt2, model_gpt2)
     return response
 
-def get_default_nlp_assessments():
+def get_demo_nlp_assessments():
     """Loads the default nlp assessment model
     The model assesses a text for toxicity
 
@@ -80,4 +97,7 @@ def get_default_nlp_assessments():
         ypred = lr_model.predict_proba(txt_embedding)
         score = ypred[0][1]
         return score
-    return {'toxicity': lr_assessment_fun}
+    def verbosity_fun(txt):
+        return len(txt)/100
+    return {'toxicity': lr_assessment_fun,
+            'verbosity': verbosity_fun}
