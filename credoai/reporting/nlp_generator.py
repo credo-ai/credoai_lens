ba34899aa81a75a4a1f1c01605076e1041edb71b
Ian Eisenberg <ian@credo.ai>
2022-02-08 23:17:53 -0800

    slight change to style. Added boxplot instead of barplot. Removed superfluous histogram

--- credoai/reporting/nlp_generator.py
+++ credoai/reporting/nlp_generator.py
@@ -8,9 +8,13 @@ from datetime import datetime
 
 
 class NLPGeneratorAnalyzerReport(CredoReport):
-    def __init__(self, module):
+    def __init__(self, module, size=5):
         super().__init__()
         self.module = module
+        self.num_gen_models = len(module.generation_functions)
+        self.num_assessment_funs = len(module.assessment_functions)
+        self.palette = plot_utils.credo_diverging_palette(self.num_gen_models)
+        self.size = size
 
     def create_report(self, filename=None):
         """Creates a fairness report for binary classification model
@@ -24,54 +28,53 @@ class NLPGeneratorAnalyzerReport(CredoReport):
         -------
         array of figures
         """
-        results_all = self.module.get_results()
-
-        # generate assessment attribute histogram plots
-        # results_all = self.results
-        num_gen_models = results_all["generation_model"].nunique()
-        for assessment_attribute in list(results_all["assessment_attribute"].unique()):
-            f = plt.figure(figsize=(8, 4), dpi=300)
-            plt.rcParams['font.size'] = 12
-            results_sub = results_all[
-                (results_all["assessment_attribute"] == assessment_attribute)
-            ][["value", "generation_model"]]
-            results_sub.reset_index(drop=True, inplace=True)
-            ax = sns.histplot(
-                data=results_sub,
-                x="value",
-                hue="generation_model",
-                element="step",
-                stat="density",
-                common_norm=False,
-                bins=20,
-                palette=plot_utils.credo_diverging_palette(num_gen_models),
-                alpha=0.7,
-            )
-            ax.set_frame_on(False)
-            plt.xlim([0, 1])
-            plt.xlabel(assessment_attribute)
-            self.figs.append(f)
-
         # Generate assessment attribute distribution parameters plots
-        fig = plt.figure(figsize=(8, 4), dpi=300)
-        plt.rcParams['font.size'] = 12
-        ax = sns.barplot(
-            x="assessment_attribute",
-            y="value",
-            hue="generation_model",
-            data=results_all,
-            palette=plot_utils.credo_diverging_palette(num_gen_models),
-            alpha=1,
-        )
-        fig.patch.set_facecolor("white")
-        ax.set_frame_on(False)
-        plt.xlabel("")
-        plt.ylabel("mean")
-        plt.legend(bbox_to_anchor=(1.25, 0.4), loc="center right", frameon=False)
-        self.figs.append(fig)
+        self.figs.append(self._plot_boxplots))
 
         # Save to pdf if requested
         if filename:
             self.export_report(filename)
 
         return self.figs
+
+    def _plot_boxplots(self):
+        with plot_utils.get_style(figure_ratio = 1/self.num_assessment_funs):
+            # Generate assessment attribute distribution parameters plots
+            fig = plt.figure()
+            sns.boxplot(x="assessment_attribute", y="value",
+                        hue="generation_model", dodge=True,
+                        data=results_all, palette=self.palette,
+                        width=.8, linewidth=2)
+            
+            sns.despine()
+            plt.xlabel("")
+            plt.ylabel("Value")
+            plt.legend(bbox_to_anchor=(1.05, 0.9))
+        return fig
+
+    def _plot_hists(self):
+        # generate assessment attribute histogram plots
+        results_all = self.module.get_results()
+        n_plots = self.num_assessment_funs
+        with plot_utils.get_style(figure_ratio = n_plots/2):
+            f, axes = plt.subplots(n_plots, 1)
+        axes = f.get_axes()
+        for i, (assessment_attribute, results_sub) in enumerate(results_all.groupby('assessment_attribute')):
+            ax = axes[i]
+            n_bins = min(results_sub.shape[0]//4, 20)
+            sns.histplot(
+                data=results_sub,
+                x="value",
+                hue="generation_model",
+                element="step",
+                stat="density",
+                common_norm=False,
+                bins=n_bins,
+                palette=self.palette,
+                alpha=0.7,
+                ax=ax
+            )
+            sns.despine()
+            plt.xlim([0, 1])
+            plt.xlabel(assessment_attribute)
+        return fig
