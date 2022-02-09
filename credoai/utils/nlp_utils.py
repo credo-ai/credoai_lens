cd7b921d0c47b2464b303a1ab66e9f1948037d58
Ian Eisenberg <ian@credo.ai>
2022-02-09 09:59:56 -0800

    sped up multiple iterations for nlp generation

--- credoai/utils/nlp_utils.py
+++ credoai/utils/nlp_utils.py
@@ -1,12 +1,13 @@
 """Requires installation of requirements-extras.txt"""
 
-from transformers import (OpenAIGPTLMHeadModel, OpenAIGPTTokenizer, 
+from transformers import (OpenAIGPTLMHeadModel, OpenAIGPTTokenizer,
                           GPT2LMHeadModel, GPT2Tokenizer)
 from credoai.data import load_lr_toxicity
 
 GENERATOR_CACHE = {}
 
-def text_generator(prompt, tokenizer, model):
+
+def text_generator(prompt, tokenizer, model, num_sequences=1, model_kwargs=None):
     """Generates text for a given prompt, tokenizer, and model
 
     Parameters
@@ -16,19 +17,34 @@ def text_generator(prompt, tokenizer, model):
     tokenizer : transformers Tokenizer
         a transformers tokenizer
     model : transformers Model
-        a transformers model     
-    
+        a transformers model    
+    num_sequences : int
+        the number of sequences to return for the input
+    generate_kwargs : dict
+        key words passed to a the text generator model 
+
     Returns
     -------
     str
         response text
-    """  
+    """
     inputs = tokenizer.encode(prompt, return_tensors='pt')
-    outputs = model.generate(inputs, max_length=max(30, len(inputs[0])+1), do_sample=True)
-    response = tokenizer.decode(outputs[0], skip_special_tokens=True)[len(prompt):]
-    return response
-    
-def gpt1_text_generator(prompt):
+    # generate responses
+    kwargs = {
+        'max_length': max(30, len(inputs[0])+1),
+        'do_sample': True,
+        'top_p': .95
+    }
+    if model_kwargs is not None:
+        kwargs.update(generate_kwargs)
+    kwargs['num_return_sequences'] = num_sequences
+    outputs = model.generate(inputs, **kwargs)
+    responses = [tokenizer.decode(o, skip_special_tokens=True)[
+        len(prompt):] for o in outputs]
+    return responses
+
+
+def gpt1_text_generator(prompt, num_sequences=1, model_kwargs=None):
     """Generates text for a given prompts using GPT1 model
 
     Parameters
@@ -40,7 +56,7 @@ def gpt1_text_generator(prompt):
     -------
     str
         response text
-    """ 
+    """
     # get or cache tokenizer
     tokenizer_gpt1 = GENERATOR_CACHE.get('tokenizer_gpt1')
     if tokenizer_gpt1 is None:
@@ -49,25 +65,28 @@ def gpt1_text_generator(prompt):
     # get or cache model
     model_gpt1 = GENERATOR_CACHE.get('model_gpt1')
     if model_gpt1 is None:
-        model_gpt1 = OpenAIGPTLMHeadModel.from_pretrained('openai-gpt', pad_token_id=tokenizer_gpt1.eos_token_id)
+        model_gpt1 = OpenAIGPTLMHeadModel.from_pretrained(
+            'openai-gpt', pad_token_id=tokenizer_gpt1.eos_token_id)
         GENERATOR_CACHE['model_gpt1'] = model_gpt1
-    response = text_generator(prompt, tokenizer_gpt1, model_gpt1)
-    return response
+    responses = text_generator(prompt, tokenizer_gpt1, model_gpt1,
+                              num_sequences=num_sequences, model_kwargs=model_kwargs)
+    return responses
 
-def gpt2_text_generator(prompt):
+
+def gpt2_text_generator(prompt, num_sequences=1, model_kwargs=None):
     """Generates text for a given prompts using GPT2 model
-    
+
     Parameters
     ----------
     prompt : str
         prompt text
-    
+
     Returns
     -------
     str
         response text
-    """   
-        # get or cache tokenizer
+    """
+    # get or cache tokenizer
     tokenizer_gpt2 = GENERATOR_CACHE.get('tokenizer_gpt2')
     if tokenizer_gpt2 is None:
         tokenizer_gpt2 = GPT2Tokenizer.from_pretrained('gpt2')
@@ -75,10 +94,13 @@ def gpt2_text_generator(prompt):
     # get or cache model
     model_gpt2 = GENERATOR_CACHE.get('model_gpt2')
     if model_gpt2 is None:
-        model_gpt2 = GPT2LMHeadModel.from_pretrained('gpt2', pad_token_id=tokenizer_gpt2.eos_token_id)
+        model_gpt2 = GPT2LMHeadModel.from_pretrained(
+            'gpt2', pad_token_id=tokenizer_gpt2.eos_token_id)
         GENERATOR_CACHE['model_gpt2'] = model_gpt2
-    response = text_generator(prompt, tokenizer_gpt2, model_gpt2)
-    return response
+    responses = text_generator(prompt, tokenizer_gpt2, model_gpt2,
+                              num_sequences=num_sequences, model_kwargs=model_kwargs)
+    return responses
+
 
 def get_demo_nlp_assessments():
     """Loads the default nlp assessment model
@@ -88,15 +110,17 @@ def get_demo_nlp_assessments():
     -------
     dict
         prediction model (sklearn.linear_model.LogisticRegression) and sentence encoding model (SentenceTransformer)
-    """   
+    """
     loaded = load_lr_toxicity()
     lr_model = loaded['model']
     st_encoder = loaded['encoder']
+
     def lr_assessment_fun(txt):
         txt_embedding = st_encoder.encode([txt])
         ypred = lr_model.predict_proba(txt_embedding)
         score = ypred[0][1]
         return score
+
     def verbosity_fun(txt):
         return len(txt)/100
     return {'toxicity': lr_assessment_fun,
