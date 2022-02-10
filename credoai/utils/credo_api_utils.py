8697b62afac876879f14a73a345efd53461283fa
Ian Eisenberg <ian@credo.ai>
2022-02-09 16:54:18 -0800

    readded tenant

--- credoai/utils/credo_api_utils.py
+++ credoai/utils/credo_api_utils.py
@@ -14,14 +14,15 @@ def read_config():
                                '.credoconfig')
     if not os.path.exists(config_file):
         # return example
-        config = {'API_KEY': 'empty'}
+        config = {'API_KEY': 'empty',
+                 'TENANT': 'empty'}
     else:
         config = dotenv_values(config_file)
     config['API_URL'] = os.path.join(config.get('CREDO_URL', CREDO_URL), "api/v1/credoai")
     return config
 
 def exchange_token():
-    data = {"api_token": CONFIG['API_KEY'], "tenant": 'credoai'}
+    data = {"api_token": CONFIG['API_KEY'], "tenant": CONFIG['TENANT']}
     headers = {'content-type': 'application/json', 'charset': 'utf-8'}
     auth_url = os.path.join(CREDO_URL, 'auth', 'exchange')
     r = requests.post(auth_url, json=data, headers=headers)
