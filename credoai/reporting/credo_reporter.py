eda5a42c700da1d95c69adb5b729eb0c205ea5db
Ian Eisenberg <ian@credo.ai>
2022-02-16 09:27:47 -0800

    results reporting (though failing for some assessments because of metric format). Also added new requirements because jupyter is necessary

--- credoai/reporting/credo_reporter.py
+++ credoai/reporting/credo_reporter.py
@@ -5,6 +5,7 @@ Defines abstract base class for all CredoReports
 from abc import ABC, abstractmethod
 from credoai.utils.common import ValidationError
 from credoai.reporting.reports import AssessmentReport
+from IPython.display import display
 import os
 import pandas as pd
 import matplotlib.backends.backend_pdf
@@ -36,8 +37,11 @@ class CredoReporter(ABC):
     {assessment_description['short']}
 
     {assessment_description['long']}
+
+    ### Results
         """
-        cells = [(description, 'markdown')] + self._create_report_cells()
+        results_table = ("reporter.display_results()", 'code')
+        cells = [(description, 'markdown'), results_table] + self._create_report_cells()
         report.add_cells(cells)
         loc = os.path.join(directory, f'assessment-{self.assessment.name}_report.ipynb')
         report.write_notebook(loc, run=True, reporter=self)
@@ -56,4 +60,21 @@ class CredoReporter(ABC):
         for fig in self.figs: ## will open an empty extra figure :(
             pdf.savefig(fig, bbox_inches='tight', pad_inches=1)
         pdf.close()
+
+    def display_results(self):
+        results = self.assessment.get_results()
+        caption_style = {
+            'selector': 'caption',
+            'props': [
+                ('font-weight', 'bold'),
+                ('font-size', '16px')
+            ]
+        }
+        for key, val in results.items():
+            try:
+                to_display=val.style.set_caption(key.upper()).set_table_styles([caption_style])
+                display(to_display)
+                print('\n\n')
+            except:
+                pass
 
