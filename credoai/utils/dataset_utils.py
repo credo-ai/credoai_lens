b3913b762ad81446e61d625513e6884649315eaa
Amin Rasekh <92744575+amrasekh@users.noreply.github.com>
2022-03-01 10:58:37 -0500

    removed concat_features_label_to_dataframe
    concat_features_label_to_dataframe() the no longer needed after the CredoData parameters redesign.

--- credoai/utils/dataset_utils.py
+++ credoai/utils/dataset_utils.py
@@ -5,31 +5,6 @@ from sklearn.feature_selection._base import SelectorMixin
 from sklearn.feature_selection import SelectKBest
 from sklearn.feature_extraction.text import CountVectorizer
 
-def concat_features_label_to_dataframe(X, y, sensitive_features):
-    """A utility method that concatenates all features and labels into a single dataframe
-
-    Returns
-    -------
-    pandas.dataframe, str, str
-        Full dataset dataframe, sensitive feature name, label name
-    """
-    if isinstance(sensitive_features, pd.Series):
-        df = pd.concat([X, sensitive_features], axis=1)
-        sensitive_feature_name = sensitive_features.name
-    else:
-        df = X.copy()
-        df['sensitive_feature'] = sensitive_features
-        sensitive_feature_name = 'sensitive_feature'
-
-    if isinstance(y, pd.Series):
-        df = pd.concat([df, y], axis=1)
-        label_name = y.name
-    else:
-        label_name = 'label'
-        df[label_name] = sensitive_features
-
-    return df, sensitive_feature_name, label_name
-
 class ColumnTransformerUtil:
     """Utility functions for ColumnTransformer
 
