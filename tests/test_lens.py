8cfa77744936c680b7fa268d61b0f39e960e27b5
Ian Eisenberg <ian@credo.ai>
2022-03-16 13:22:26 -0700

    reverted back to old import strategy with cl

--- tests/test_lens.py
+++ tests/test_lens.py
@@ -1,4 +1,4 @@
-import credoai.artifacts as ca
+
 import numpy as np
 import pandas as pd
 
@@ -13,17 +13,17 @@ y = data['target']
 sensitive_feature = data['data']["gender"]
 
 data = pd.concat([data['data'], y], axis=1)
-credo_data = ca.CredoData(
+credo_data = cl.CredoData(
     name="income_data", data=data, label_key='income', sensitive_feature_key='gender'
 )
 
 model = LogisticRegression(random_state=0).fit(X, y)
-credo_model = ca.CredoModel(name="income_classifier", model=model)
+credo_model = cl.CredoModel(name="income_classifier", model=model)
 alignment_spec = {"FairnessBase": {"metrics": ["precision_score"]},
                     "PerformanceBase": {"metrics": ["precision_score"]}}
 
 def test_lens_with_model():
-    lens = Lens(model=credo_model, data=credo_data, spec=alignment_spec)
+    lens = cl.Lens(model=credo_model, data=credo_data, spec=alignment_spec)
 
     results = lens.run_assessments().get_results()
     metric = results["FairnessBase"]["fairness"].index[0]
@@ -35,7 +35,7 @@ def test_lens_with_model():
     assert lens.assessments['FairnessBase'].initialized_module.metrics == ['precision_score']
 
 def test_lens_without_model():
-    lens = Lens(data=credo_data)
+    lens = cl.Lens(data=credo_data)
     results = lens.run_assessments().get_results()
     metric_score = results['DatasetFairness']["demographic_parity_ratio"][0]['value']
     assert metric_score == 0.5
@@ -49,17 +49,17 @@ def test_lens_dataset_with_missing_data():
     sensitive_feature = data['data']["gender"]
 
     data = pd.concat([data['data'], y], axis=1)
-    credo_data = ca.CredoData(
+    credo_data = cl.CredoData(
         name="income_data", data=data, label_key='income', sensitive_feature_key='gender'
     )
 
-    lens = Lens(data=credo_data)
+    lens = cl.Lens(data=credo_data)
     results = lens.run_assessments().get_results()
     metric_score = results['DatasetFairness']["demographic_parity_ratio"][0]['value']
     assert metric_score == 0.375
     assert set(lens.assessments.keys()) == {'DatasetFairness'} 
 
 def test_report_creation():
-    lens = Lens(model=credo_model, data=credo_data, spec=alignment_spec)
+    lens = cl.Lens(model=credo_model, data=credo_data, spec=alignment_spec)
     lens.run_assessments()
     out = lens.create_report()
