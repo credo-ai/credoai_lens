26859a0b6abd4904b71f2d93081d678ff3090d25
chang <chang@credo.ai>
2022-08-02 18:43:26 +0900

    Apply CredoApiClient to credo_api_utils

--- credoai/utils/credo_api_utils.py
+++ credoai/utils/credo_api_utils.py
@@ -1,79 +1,17 @@
-import json
-import os
-import time
 from collections import defaultdict
-
 import requests
-from credoai.utils.common import IntegrationError, get_project_root, json_dumps
-from credoai.utils.constants import CREDO_URL
-from dotenv import dotenv_values
-from json_api_doc import deserialize, serialize
+from credoai.utils.common import IntegrationError
+from credoai.utils.credo_api_client import CredoApiClient
 
-
-def read_config():
-    config_file = os.path.join(os.path.expanduser("~"), ".credoconfig")
-    if not os.path.exists(config_file):
-        # return example
-        config = {"API_KEY": "empty", "TENANT": "empty"}
-    else:
-        config = dotenv_values(config_file)
-    config["API_URL"] = os.path.join(
-        config.get("CREDO_URL", CREDO_URL), f"api/v1/{config['TENANT']}"
-    )
-    return config
-
-
-def exchange_token():
-    data = {"api_token": CONFIG["API_KEY"], "tenant": CONFIG["TENANT"]}
-    headers = {"content-type": "application/json", "charset": "utf-8"}
-    auth_url = os.path.join(CONFIG.get("CREDO_URL", CREDO_URL), "auth", "exchange")
-    r = requests.post(auth_url, json=data, headers=headers)
-    return f"Bearer {r.json()['access_token']}"
-
-
-def refresh_token():
-    global HEADERS
-    key = exchange_token()
-    HEADERS["Authorization"] = key
-    SESSION.headers.update(HEADERS)
-
-
-def renew_access_token(func):
-    def wrapper(*args, **kwargs):
-        response = func(*args, **kwargs)
-        if response.status_code == 401:
-            refresh_token()
-            response = func(*args, **kwargs)
-        response.raise_for_status()
-        return response
-
-    return wrapper
-
-
-CONFIG = read_config()
-HEADERS = {"Authorization": None, "accept": "application/vnd.api+json"}
-SESSION = requests.Session()
-SESSION.headers.update(HEADERS)
-
-
-def get_end_point(end_point):
-    return os.path.join(CONFIG["API_URL"], end_point)
-
-
-@renew_access_token
-def submit_request(request, end_point, **kwargs):
-    response = SESSION.request(request, end_point, **kwargs)
-    return response
+client = CredoApiClient()
 
 
 def get_assessment(assessment_id):
-    end_point = get_end_point(f"use_case_assessments/{assessment_id}")
-    return deserialize(submit_request("get", end_point).json())
+    return client.get(f"use_case_assessments/{assessment_id}")
 
 
 def get_associated_models(use_case_id):
-    end_point = get_end_point(f"use_cases/{use_case_id}?include=models")
-    return deserialize(submit_request("get", end_point).json())["models"]
+    return client.get(f"use_cases/{use_case_id}?include=models")
 
 
 def get_assessment_spec(assessment_spec_url):
@@ -84,9 +22,9 @@ def get_assessment_spec(assessment_spec_url):
     the assessment plan
     """
     try:
-        end_point = get_end_point(assessment_spec_url)
-        downloaded_spec = deserialize(submit_request("get", end_point).json())
-        assessment_spec = {k: v for k, v in downloaded_spec.items() if "_id" in k}
+        downloaded_spec = client.get(assessment_spec_url)
+        assessment_spec = {k: v for k,
+                           v in downloaded_spec.items() if "_id" in k}
         assessment_spec["assessment_plan"] = downloaded_spec["assessment_plan"]
         assessment_spec["policy_questions"] = _process_policies(
             downloaded_spec["policies"]
@@ -171,14 +109,9 @@ def get_use_case_by_name(use_case_nmae):
 
 
 def post_assessment(use_case_id, model_id, data):
-    end_point = get_end_point(f"use_cases/{use_case_id}/models/{model_id}/assessments")
-    request = submit_request(
-        "post",
-        end_point,
-        data=json_dumps(serialize(data)),
-        headers={"content-type": "application/vnd.api+json"},
-    )
-    assessment_id = deserialize(request.json())["id"]
+    response = client.post(
+        f"use_cases/{use_case_id}/models/{model_id}/assessments", data)
+    assessment_id = response["id"]
     return get_assessment(assessment_id)
 
 
@@ -196,10 +129,8 @@ def register_dataset(dataset_name):
         Dictionary with Identifiers for dataset
         on Credo AI's Governance App
     """
-    end_point = get_end_point(f"datasets")
     project = {"name": dataset_name, "$type": "string"}
-    data = json.dumps(serialize(project))
-    response = _register_artifact(data, end_point)
+    response = _register_artifact(project, "datasets")
     return {"name": dataset_name, "dataset_id": response["id"]}
 
 
@@ -217,54 +148,29 @@ def register_model(model_name):
         Dictionary with Identifiers for Model and Project
         on Credo AI's Governance App
     """
-    end_point = get_end_point(f"models")
     model = {"name": model_name, "version": "1.0", "$type": "string"}
-    data = json.dumps(serialize(model))
-    response = _register_artifact(data, end_point)
+    response = _register_artifact(model, "models")
     return {"name": model_name, "model_id": response["id"]}
 
 
 def register_model_to_usecase(use_case_id, model_id):
-    data = {"data": [{"id": model_id, "type": "models"}]}
-    end_point = get_end_point(f"use_cases/{use_case_id}/relationships/models")
-    submit_request(
-        "post",
-        end_point,
-        data=json.dumps(data),
-        headers={"content-type": "application/vnd.api+json"},
-    )
+    data = [{"id": model_id, "$type": "models"}]
+    client.post(f"use_cases/{use_case_id}/relationships/models", data)
 
 
 def register_dataset_to_model(model_id, dataset_id):
-    data = {"data": {"id": dataset_id, "type": "datasets"}}
-    end_point = get_end_point(f"models/{model_id}/relationships/dataset")
-    submit_request(
-        "patch",
-        end_point,
-        data=json.dumps(data),
-        headers={"content-type": "application/vnd.api+json"},
-    )
+    data = {"id": dataset_id, "$type": "datasets"}
+    client.patch(f"models/{model_id}/relationships/dataset", data)
 
 
 def register_dataset_to_model_usecase(use_case_id, model_id, dataset_id):
-    data = serialize({"dataset_id": dataset_id, "$type": "string", "id": "resource-id"})
-    end_point = get_end_point(f"use_cases/{use_case_id}/models/{model_id}/config")
-    submit_request(
-        "patch",
-        end_point,
-        data=json.dumps(data),
-        headers={"content-type": "application/vnd.api+json"},
-    )
+    data = {"dataset_id": dataset_id, "$type": "string", "id": "resource-id"}
+    client.patch(f"use_cases/{use_case_id}/models/{model_id}/config", data)
 
 
 def _register_artifact(data, end_point):
     try:
-        response = submit_request(
-            "post",
-            end_point,
-            data=data,
-            headers={"content-type": "application/vnd.api+json"},
-        )
+        return client.post(end_point, data)
     except requests.exceptions.HTTPError as err:
         if err.response.status_code == 422:
             raise IntegrationError(
@@ -272,14 +178,12 @@ def _register_artifact(data, end_point):
             )
         else:
             raise
-    return deserialize(response.json())
 
 
 def _get_by_name(name, endpoint):
     """Given a name, return the id"""
-    end_point = get_end_point(endpoint)
     params = {"filter[name][value]": name, "filter[name][type]": "match"}
-    returned = deserialize(submit_request("get", end_point, params=params).json())
+    returned = client.get(endpoint, params=params)
     if len(returned) == 1:
         return returned[0]
     return None
@@ -288,11 +192,12 @@ def _get_by_name(name, endpoint):
 def _get_name(artifact_id, artifact_type):
     """Given an ID, return the name"""
     try:
-        end_point = get_end_point(f"{artifact_type}/{artifact_id}")
-        return deserialize(submit_request("get", end_point).json())["name"]
+        response = client.get(f"{artifact_type}/{artifact_id}")
+        return response["name"]
     except requests.exceptions.HTTPError as err:
         if err.response.status_code == 400:
-            raise IntegrationError(f"No {artifact_type} found with id: {artifact_id}")
+            raise IntegrationError(
+                f"No {artifact_type} found with id: {artifact_id}")
 
 
 def _process_policies(policies):
