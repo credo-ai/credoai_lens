727b96dfded3ac2b19e95e905fd34f5be41889ff
Ian Eisenberg <ian@credo.ai>
2022-08-11 11:23:33 -0400

    updated training scrip to use train/test split

--- docs/notebooks/training_script.py
+++ docs/notebooks/training_script.py
@@ -1,14 +1,23 @@
 # imports for example data and model training
 from credoai.data import fetch_creditdefault
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
 
 data = fetch_creditdefault()
 df = data["data"]
 df["target"] = data["target"].astype(int)
-sensitive_features = df["SEX"]
 
 # fit model
 model = RandomForestClassifier()
 X = df.drop(columns=["SEX", "target"])
 y = df["target"]
-model.fit(X, y)
+sensitive_features = df["SEX"]
+(
+    X_train,
+    X_test,
+    y_train,
+    y_test,
+    sensitive_features_train,
+    sensitive_features_test,
+) = train_test_split(X, y, sensitive_features, random_state=42)
+model.fit(X_train, y_train)
