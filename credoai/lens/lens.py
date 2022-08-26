3fe4f58b2faf8ba7f6205d326f4d5518502c6924
Ian Eisenberg <ian@credo.ai>
2022-08-25 18:17:49 -0700

    updated pipeline

--- credoai/lens/lens.py
+++ credoai/lens/lens.py
@@ -1,12 +1,12 @@
-from dataclasses import dataclass
 import inspect
+import uuid
+from dataclasses import dataclass
 from typing import List, Type, Union
-from credoai.artifacts import CredoGovernance, CredoModel, CredoData
-from credoai.utils.common import (
-    ValidationError,
-)
+
 from absl import logging
+from credoai.artifacts import CredoData, CredoGovernance, CredoModel
 from credoai.evaluators.evaluator import Evaluator
+from credoai.utils.common import ValidationError
 
 ## TODO: Define what format is the assessment plan coming from
 ## the platform.
@@ -23,14 +23,13 @@ def set_logging_level(logging_level):
     logging.set_verbosity(logging_level)
 
 
-@dataclass
 class Lens:
     def __init__(
         self,
-        model: CredoModel = (None,),
-        data: CredoData = (None,),
-        training_data: CredoData = (None,),
-        governance: Union[CredoGovernance, str] = (None,),
+        model: Model = (None,),
+        data: Data = (None,),
+        training_data: Data = (None,),
+        governance: Union[Governance, str] = (None,),
         logging_level: Union[str, int] = "info",
     ) -> None:
         self.model = model
@@ -40,16 +39,19 @@ class Lens:
         self.run_time = False
         self.gov = None
         self.governance = governance
-        self.pipeline = []
-        self.pipeline_steps = []
+        self.pipeline = {}
         self.pipeline_results = []
         self._validate()
         ## TODO: evaluate what library to use for logging
         set_logging_level(logging_level)
 
-    def add(self, evaluator, id: str, metadata: dict = None):
+    def __call__(self):
+        self._create_pipeline()
+        self.run()
+
+    def add(self, evaluator, id: str = None, metadata: dict = None):
         ## Validate same identifier doesn't exist already
-        if id in self.pipeline_steps:
+        if id in self.pipeline:
             raise ValueError(
                 f"An evaluator with id: {id} is already in the pipeline. Id has to be unique"
             )
@@ -57,25 +59,23 @@ class Lens:
         if not isinstance(evaluator, Evaluator):
             ## TODO: Make sure instance is correct after evaluator type is defined
             raise TypeError(
-                f"Evaluator has to be of type evaluator...not {type(evaluator)}"
+                f"Evaluator has to be of type evaluator... not {type(evaluator)}"
             )
-        self.pipeline.append({"id": id, "evaluator": evaluator, "meta": metadata})
-        self.pipeline_steps.append(id)
+        if id is None:
+            id = f"{evaluator.name}_{str(uuid.uuid1())[:6]}"
+        self.pipeline[id] = {"evaluator": evaluator, "meta": metadata}
         return self
 
     def remove(self, id: str):
         # Find position
-        step_to_remove = self.pipeline_steps.index(id)
-        # Remove from both lists
-        self.pipeline.pop(step_to_remove)
-        self.pipeline_steps.pop(step_to_remove)
+        del self.pipeline[id]
         return self
 
     def run(self):
         """
         Run the main loop across all the pipeline steps
         """
-        for step in self.pipeline_steps:
+        for step in self.pipeline:
             ## TODO: formalize when internal evidence class is completed how to pass metadata
             step["evaluator"].evaluate()
             ## TODO check results is the actual attribute in the evaluator class
@@ -96,6 +96,10 @@ class Lens:
         1. Convert internal evidence to platform evidence
         2. Push to platform, main code to do that should be in the governance folder
         """
+        pass
+
+    def _create_pipeline(self):
+        """Automatically generate pipeline"""
         pass
 
     def _validate(self):
