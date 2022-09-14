890278ce876a7ce18e884885bb7a499d1344d8f6
chang <chang@credo.ai>
2022-09-15 01:19:13 +0900

    Remove stale tests

--- tests/test_imports.py
+++
@@ -1,6 +0,0 @@
-from credoai.assessment import *
-from credoai.integration import *
-from credoai.lens import *
-from credoai.modules import *
-from credoai.reporting import *
-from credoai.utils import *
--- tests/test_version.py
+++
@@ -1,6 +0,0 @@
-import pytest
-from credoai import __version__
-
-
-def test_version():
-    assert isinstance(__version__, str)
