bec12091bfc981568a23c7bb4c6db2f11db07fa1
fabrizio-credo <fabrizio@credo.ai>
2022-09-19 10:41:29 -0700

    Wiped out old tests, restarting from blank state

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -1,6 +1,3 @@
-from credoai.assessment import *
-from credoai.integration import *
 from credoai.lens import *
 from credoai.modules import *
-from credoai.reporting import *
 from credoai.utils import *
