40264d69946bacbb70685c1ac58c973e66a116c3
Ian Eisenberg <ian@credo.ai>
2022-09-27 11:38:53 -0700

    updated tests using pytest syntax

--- tests/utils/test_utils.py
+++ tests/utils/test_utils.py
@@ -1,25 +1,22 @@
+import pytest
 from credoai.utils import check_subset
 
 
-def test_check_subset():
-    superset = {"a": 1, "b": 2, "c": [3, 4], "d": 5}
-    subset = {"a": 1}
-    assert check_subset(subset, superset) == True
-
-    subset = {"a": 1, "c": [3]}
-    assert check_subset(subset, superset) == True
-
-    subset = {"a": 1, "c": [3, 5]}
-    assert check_subset(subset, superset) == False
-
-    subset = {"a": 1, "e": 5}
-    assert check_subset(subset, superset) == False
-
-    subset = {}
-    assert check_subset(subset, superset) == True
-
-    assert check_subset({}, []) == False
-
-    superset = {"a": 1, "b": 2, "c": [3, 4], "d": {"e": [5, 6, 7], "f": 8}}
-    subset = {"a": 1, "d": {"e": [5, 6]}}
-    assert check_subset(subset, superset) == True
+@pytest.mark.parametrize(
+    "superset,subset,expected",
+    [
+        ({"a": 1, "b": 2, "c": [3, 4], "d": 5}, {"a": 1}, True),
+        ({"a": 1, "b": 2, "c": [3, 4], "d": 5}, {"a": 1, "c": [3]}, True),
+        ({"a": 1, "b": 2, "c": [3, 4], "d": 5}, {"a": 1, "c": [3, 5]}, False),
+        ({"a": 1, "b": 2, "c": [3, 4], "d": 5}, {"a": 1, "e": 5}, False),
+        ({"a": 1, "b": 2, "c": [3, 4], "d": 5}, {}, True),
+        ([], {}, False),
+        (
+            {"a": 1, "b": 2, "c": [3, 4], "d": {"e": [5, 6, 7], "f": 8}},
+            {"a": 1, "d": {"e": [5, 6]}},
+            True,
+        ),
+    ],
+)
+def test_check_subset(superset, subset, expected):
+    assert check_subset(subset, superset) == expected
