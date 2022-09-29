45fd68c633d830be6d79f00b5730dfc761ae2c56
Ian Eisenberg <ian@credo.ai>
2022-09-28 17:57:35 -0700

    removed requirements separation

--- credoai/datasets/__init__.py
+++ credoai/datasets/__init__.py
@@ -5,12 +5,4 @@ Data, models and other static resources needed for CredoAI lens
 from ._fetch_censusincome import fetch_censusincome
 from ._fetch_creditdefault import fetch_creditdefault
 from ._fetch_testdata import fetch_testdata
-
-__all__ = ["fetch_creditdefault", "fetch_censusincome"]
-
-try:
-    from ._load_lr_toxicity import *
-
-    __all__.append("load_lr_toxicity")
-except ModuleNotFoundError:
-    pass
+from ._load_lr_toxicity import *
