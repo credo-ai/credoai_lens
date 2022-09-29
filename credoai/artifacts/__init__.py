7d5c9083c9be80dcdd9ab73219d23fa2e6ab6379
fabrizio-credo <fabrizio@credo.ai>
2022-09-29 11:36:53 -0700

    Added docstrings across the artifact module

--- credoai/artifacts/__init__.py
+++ credoai/artifacts/__init__.py
@@ -1,3 +1,10 @@
+"""
+Containers for different types of data and models
+
+In order to ensure Lens running correctly across a variety of data formats
+and model types, user's inputs are wrapped into classes that format/validate
+them.
+"""
 from .data.base_data import Data
 from .data.tabular_data import TabularData
 from .model.base_model import Model
