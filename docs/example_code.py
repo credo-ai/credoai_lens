3f3083366ea81e4db0ef059e552ff1d907769163
fabrizio-credo <fabrizio@credo.ai>
2022-09-30 08:38:15 -0700

    Removed basic example from boilerplate

--- docs/example_code.py
+++ docs/example_code.py
@@ -1,70 +1,32 @@
 """
-Boilerplate code for basic Lens functionality
+Boilerplate code for connecting Lens to Credo AI Platform
+For more information check: https://credoai-lens.readthedocs.io/en/release-1.0.0/notebooks/governance_integration.html
 
 Requirements
 ------------
 
+    Platform
+    --------
+    1. A registered use case exists on the Platform
+    2. A token and the relative .credoconfig file have been generated
+
+    Local environment
+    -----------------
     1. Credoai is correctly installed in the environment
-    2. User has access to datasets/ fitted model`
+    2. User has access to datasets/ fitted model
+    3. Location of the .credoconfig file is the home directory: ~/
 
-Examples
---------
-    1. Basic run without connection to Credo AI Platform
-    For more information check: https://credoai-lens.readthedocs.io/en/release-1.0.0/notebooks/quickstart.html
+"""
 
-    2. Run including connection to Credo AI Platform
-    For more information check: https://credoai-lens.readthedocs.io/en/release-1.0.0/notebooks/governance_integration.html
+########################################################################
+
+"""
+Example: Run assessment plan downloaded from Credo AI Platform
 
 Assumptions
 -----------
     1. Model is a classification model
-    2. Data is in tabular form
-
-"""
-
-########################################################################
-
-"""
-Example 1: Evaluate Model Fairness
-"""
-
-from credoai.lens import Lens
-from credoai.artifacts import ClassificationModel, TabularData
-from credoai.evaluators import ModelFairness
-
-# Create artifacts needed for Lens
-credo_model = ClassificationModel(
-    name="your model name",
-    model_like="<your model>",
-)
-credo_data = TabularData(
-    name="your data name",
-    X="<your X data object>",
-    y="<your y data object>",
-    sensitive_features="<your sensitive features object>",
-)
-
-# Initialization of the Lens object
-lens = Lens(model=credo_model, assessment_data=credo_data)
-
-# Initialize the evaluator and add it to Lens
-metrics = ["precision_score", "recall_score", "equal_opportunity"]
-lens.add(ModelFairness(metrics=metrics), id="MyModelFairness")
-
-# run Lens
-lens.run()
-lens.get_results()["MyModelFairness"][0]
-
-########################################################################
-
-"""
-Example 2: Run assessment plan downloaded from Credo AI Platform
-
-Assumptions
------------
-    1. A registered use case exists on the Platform
-    2. A token and the relative .credoconfig file have been generated
-    3. Location of the config file is the home directory: ~/
+    2. Data is in tabular form    
 """
 
 
