c1ec80cb27608492203466486021dd90537a5e9a
Ian Eisenberg <ian@credo.ai>
2022-09-30 09:52:07 -0700

    added some comments to example code

--- docs/example_code.py
+++ docs/example_code.py
@@ -13,7 +13,7 @@ Requirements
     Local environment
     -----------------
     1. Credoai is correctly installed in the environment
-    2. User has access to datasets/ fitted model
+    2. User has access to training and/or assessment data, and a fitted model
     3. Location of the .credoconfig file is the home directory: ~/
 
 """
@@ -30,9 +30,9 @@ Assumptions
 """
 
 
+from credoai.artifacts import ClassificationModel, TabularData
 from credoai.governance import Governance
 from credoai.lens import Lens
-from credoai.artifacts import ClassificationModel, TabularData
 
 # Get your assessment plan
 gov = Governance()
@@ -40,6 +40,9 @@ url = "<your assessment plan url>"
 gov.register(assessment_plan_url=url)
 
 # Create artifacts needed for Lens
+# **Note**
+# Different models or datasets may need different Model/Data Classes
+# e.g., RegressionModel instead of ClassificationModel
 credo_model = ClassificationModel(
     name="your model name",
     model_like="<your model>",
@@ -52,9 +55,11 @@ credo_data = TabularData(
 )
 
 # Pass governance to Lens at init
+# Under the hood, a pipeline of evaluators is created based
+# on the governance requirements
 lens = Lens(model=credo_model, assessment_data=credo_data, governance=gov)
 lens.run()
 
 # Add evidence and export to Credo AI Platform
-lens.send_to_governance(True)
+lens.send_to_governance()
 gov.export()
