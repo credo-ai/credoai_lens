bf5172c40a20b23bacec083ed963d746bd87ac04
Amin Rasekh <92744575+amrasekh@users.noreply.github.com>
2022-10-03 16:57:48 -0400

    removed nlp scripts

--- credoai/utils/__init__.py
+++ credoai/utils/__init__.py
@@ -6,9 +6,3 @@ from .common import *
 from .dataset_utils import *
 from .logging import *
 from .model_utils import *
-
-# for modules that have "extra" requirements
-try:
-    import nlp_utils
-except:
-    pass
