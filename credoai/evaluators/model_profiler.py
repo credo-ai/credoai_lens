04b11932cba70b29d2983c784934a85f81153069
fabrizio-credo <fabrizio@credo.ai>
2022-10-17 14:18:00 -0700

    Added data sample to model card

--- credoai/evaluators/model_profiler.py
+++ credoai/evaluators/model_profiler.py
@@ -1,9 +1,11 @@
 from typing import Optional
+
 from pandas import DataFrame
+
 from credoai.evaluators import Evaluator
-from credoai.evaluators.utils.validation import check_requirements_existence
+from credoai.evaluators.utils.validation import check_existence
 from credoai.evidence.containers import ModelProfilerContainer
-from credoai.utils.common import ValidationError
+from credoai.utils import ValidationError, global_logger
 
 USER_INFO_TEMPLATE = {
     "developed_by": None,
@@ -38,6 +40,14 @@ class ModelProfiler(Evaluator):
     This evaluator builds a model card the purpose of which is to characterize
     a fitted model.
 
+    The overall strategy is:
+        1. Extract all potentially useful info from the model itself in an
+            automatic fashion.
+        2. Allow the user to personalize the model card freely.
+
+    The method generate_template() provides a dictionary with several entries the
+    user could be interested in filling up.
+
     Parameters
     ----------
     model_info : Optional[dict], optional
@@ -52,22 +62,15 @@ class ModelProfiler(Evaluator):
     """
 
     name = "ModelProfiler"
-    required_artifacts = {"model"}
+    required_artifacts = {"model", "assessment_data"}
 
     def __init__(self, model_info: Optional[dict] = None):
-        """
-        _summary_
-
-        Parameters
-        ----------
-        model_info : Optional[dict], optional
-            _description_, by default None
-        """
         super().__init__()
         self.usr_model_info = model_info
         if not self.usr_model_info:
             self.usr_model_info = {}
         self._validate_usr_model_info()
+        self.logger = global_logger
 
     def _setup(self):
         self.model_name = self.model.name
@@ -75,14 +78,15 @@ class ModelProfiler(Evaluator):
         self.model_type = type(self.model)
 
     def _validate_arguments(self):
-        check_requirements_existence(self)
+        check_existence(self.model, "model")
 
     def evaluate(self):
         # Collate info
         basic = self._get_basic_info()
         res = self._get_model_params()
         self.usr_model_info = {k: v for k, v in self.usr_model_info.items() if v}
-        res = {**basic, **res, **self.usr_model_info}
+        data_sample = self._get_dataset_sample()
+        res = {**basic, **res, **self.usr_model_info, **data_sample}
         # Format
         res = DataFrame.from_dict(res, orient="index")
         res.columns = ["results"]
@@ -104,6 +108,23 @@ class ModelProfiler(Evaluator):
             "python_model_type": str(self.model_type).split("'")[1],
         }
 
+    def _get_dataset_sample(self) -> dict:
+        """
+        If assessment data is available get a sample of it.
+        """
+        try:
+            data_sample = {
+                "data_sample": self.assessment_data.X.sample(
+                    3, random_state=42
+                ).to_dict(orient="list")
+            }
+            return data_sample
+
+        except:
+            message = "No data found -> a sample of the data won't be included in the model card"
+            self.logger.info(message)
+            return {}
+
     def _get_model_params(self) -> dict:
         """
         Select which parameter structure to utilize based on library/model used.
@@ -115,6 +136,11 @@ class ModelProfiler(Evaluator):
         """
         if "sklearn" in str(self.model_type):
             return self._get_sklearn_model_params()
+        else:
+            self.logger.info(
+                "Automatic model parameter inference not available for this model type."
+            )
+            return {}
 
     def _get_sklearn_model_params(self) -> dict:
         """
@@ -138,7 +164,7 @@ class ModelProfiler(Evaluator):
 
     def _validate_usr_model_info(self):
         """
-        Validate user model info.
+        Validate information that the user has inputted manually.
 
         Any key is valid unless it's already in use internally.
 
