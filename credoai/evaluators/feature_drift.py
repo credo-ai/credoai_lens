5490a47eddb71c369ecd0fe3439114c83821c300
fabrizio-credo <fabrizio@credo.ai>
2022-10-21 11:27:27 -0700

    Doc completed

--- credoai/evaluators/feature_drift.py
+++ credoai/evaluators/feature_drift.py
@@ -1,3 +1,4 @@
+"""Feature Drift evaluator"""
 from pandas import DataFrame, Series
 
 from credoai.artifacts import ClassificationModel
@@ -12,10 +13,35 @@ class FeatureDrift(Evaluator):
     """
     Measure Feature Drift using population stability index
 
-    This evaluator
+    This evaluator measures feature drift in:
+
+    1. Model prediction: the prediction for the assessment dataset is compared
+        to the prediction for the training dataset.
+        In the case of classifiers, the prediction is performed with predict proba if available.
+        If it is not available, the prediction is treated like a categorical variable, see the
+        processing of categorical variables in the item below.
+
+    2. Dataset features: 1 to 1 comparison across all features for the datasets.
+        - Numerical features are directly fed into the population_stability_index metric, and
+        binned according to the parameters specified at init time.
+        - Categorical features percentage distribution is manually calculated. The % amount of
+        samples per each class is calculated and then fed into the population_stability_index metric.
+        The percentage flag in the metric is set to True, to bypass the internal binning process.
+
+
+    Parameters
+    ----------
+    buckets : int, optional
+        Number of buckets to consider to bin the predictions, by default 10
+    buckettype :  Literal["bins", "quantiles"]
+        Type of strategy for creating buckets, bins splits into even splits,
+        quantiles splits into quantiles buckets, by default "bins"
+    csi_calculation : bool, optional
+        _description_, by default False
     """
 
-    def __init__(self, buckets=10, buckettype="bins", csi_calculation=False):
+    def __init__(self, buckets: int = 10, buckettype="bins", csi_calculation=False):
+
         self.bucket_number = buckets
         self.buckettype = buckettype
         self.csi_calculation = csi_calculation
@@ -93,8 +119,8 @@ class FeatureDrift(Evaluator):
             train_data = self.training_data.X[col_name]
             assess_data = self.assessment_data.X[col_name]
             if self.assessment_data.X[col_name].dtype == "category":
-                train, asess = self._create_bin_percentage(train_data, assess_data)
-                psis[col_name] = population_stability_index(train, asess, True)
+                train, assess = self._create_bin_percentage(train_data, assess_data)
+                psis[col_name] = population_stability_index(train, assess, True)
             else:
                 psis[col_name] = population_stability_index(train_data, assess_data)
         psis = DataFrame.from_dict(psis, orient="index")
