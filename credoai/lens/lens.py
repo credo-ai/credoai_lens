650a86cefa05ed9fccedd979452917b9302b71e6
fabrizio-credo <fabrizio@credo.ai>
2022-10-27 15:54:59 -0700

    Modified lens to issue warning if tags are requested and not found

--- credoai/lens/lens.py
+++ credoai/lens/lens.py
@@ -11,8 +11,7 @@ from credoai.artifacts import Data, Model
 from credoai.evaluators.evaluator import Evaluator
 from credoai.governance import Governance
 from credoai.lens.pipeline_creator import PipelineCreator
-from credoai.utils import (ValidationError, check_subset, flatten_list,
-                           global_logger)
+from credoai.utils import ValidationError, check_subset, flatten_list, global_logger
 
 # Custom type
 Pipeline = List[Union[Evaluator, Tuple[Evaluator, str, dict]]]
@@ -415,6 +414,11 @@ class Lens:
                     raise ValidationError(
                         "Sensitive features should have the same shape across assessment and training data"
                     )
+
+        if self.model is not None and self.gov is not None:
+            if self.model.tags not in self.gov._unique_tags:
+                mes = f"Model tags: {self.model.tags} are not among the once found in the governance object: {self.gov._unique_tags}"
+                self.logger.warning(mes)
 
     @staticmethod
     def _consume_pipeline_step(step):
