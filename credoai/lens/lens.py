5d39b1db0b9b9374b1d2a3a504792e63124f4ca4
esherman-credo <esherman@credo.ai>
2022-11-08 18:40:28 -0500

    Switched to deep copy of pipeline step rather than evaluator so that we also copy the inputted metadata

--- credoai/lens/lens.py
+++ credoai/lens/lens.py
@@ -393,12 +393,12 @@ class Lens:
         for step in pipeline:
             if not isinstance(step, tuple):
                 step = (step,)
-            evaltr, meta = self._consume_pipeline_step(step)
+            evaltr, meta = self._consume_pipeline_step(deepcopy(step))
             if isclass(evaltr):
                 raise ValidationError(
                     f"Evaluator in step {step} needs to be instantiated"
                 )
-            self.add(deepcopy(evaltr), meta)
+            self.add(evaltr, meta)
         return self
 
     def _validate(self):
