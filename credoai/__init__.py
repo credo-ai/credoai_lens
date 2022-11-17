b7406dac2b29fb55b293d42d909d3ba11e82efa9
esherman-credo <esherman@credo.ai>
2022-11-17 11:18:44 -0500

    Add functionality to check version of Lens on import. Compare to PyPI

--- credoai/__init__.py
+++ credoai/__init__.py
@@ -2,4 +2,14 @@
 Primary interface for Credo AI Lens package
 """
 
-__version__ = "1.1.2"
+from importlib.metadata import version
+
+# get package version programatically from installed wheel
+# does not rely on update to __init__.py for each new release
+# __version__ = version("credoai-lens")
+__version__ = "1.0.0"
+
+
+from credoai.utils.version_check import validate_version
+
+validate_version()
---
+++ credoai/utils/version_check.py
@@ -0,0 +1,23 @@
+import requests
+import credoai
+from credoai.utils import global_logger
+
+
+def validate_version():
+    current_version = credoai.__version__
+
+    package = "credoai-lens"  # replace with the package you want to check
+    response = requests.get(f"https://pypi.org/pypi/{package}/json")
+    latest_version = response.json()["info"]["version"]
+
+    on_latest = current_version == latest_version
+
+    if not on_latest:
+        global_logger.warning(
+            """
+            You are using credoai-lens version %s, however a newer version is available.
+            Lens is updated regularly with major improvements and bug fixes.
+            Please upgrade via the command: "python -m pip install --upgrade credoai-lens"
+            """,
+            current_version,
+        )
