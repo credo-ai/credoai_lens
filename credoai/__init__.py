a4d38a3b6b5a3f2f0d48871f09fe66b9867256d6
esherman-credo <esherman@credo.ai>
2022-11-18 09:44:55 -0500

    Revert __init__ to working condition

--- credoai/__init__.py
+++ credoai/__init__.py
@@ -2,12 +2,7 @@
 Primary interface for Credo AI Lens package
 """
 
-from importlib.metadata import version
-
-# get package version programatically from installed wheel
-# does not rely on update to __init__.py for each new release
-# __version__ = version("credoai-lens")
-__version__ = "1.0.0"
+__version__ = "1.1.2"
 
 
 from credoai.utils.version_check import validate_version
