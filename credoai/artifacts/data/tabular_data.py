22c65d17e3a1316928c59409bfe6662809e67422
Ian Eisenberg <ian@credo.ai>
2022-12-05 14:00:01 -0800

    update validation functions

--- credoai/artifacts/data/tabular_data.py
+++ credoai/artifacts/data/tabular_data.py
@@ -14,14 +14,14 @@ class TabularData(Data):
     """Class wrapper around tabular data
 
     TabularData serves as an adapter between tabular datasets
-    and the evaluators in Lens.
+    and the evaluators in Lens. TabularData processes X
 
     Parameters
     -------------
     name : str
         Label of the dataset
     X : array-like of shape (n_samples, n_features)
-        Dataset
+        Dataset. Must be able to be transformed into a pandas DataFrame
     y : array-like of shape (n_samples, n_outputs)
         Outcome
     sensitive_features : pd.Series, pd.DataFrame, optional
@@ -52,16 +52,20 @@ class TabularData(Data):
         return deepcopy(self)
 
     def _process_X(self, X):
-        """Standardize X data"""
+        """Standardize X data
+
+        Ensures X is a dataframe with string-named columns
+        """
         temp = pd.DataFrame(X)
         # Column names are converted to strings, to avoid mixed types
         temp.columns = temp.columns.astype("str")
         return temp
 
     def _process_y(self, y):
-        """Standardize y data"""
-        # if X is pandas object, and y is convertible, convert y to
-        # pandas object with X's index
+        """Standardize y data
+
+        If y is convertible, convert y to pandas object with X's index
+        """
         if isinstance(y, (np.ndarray, list)):
             pd_type = pd.Series
             if isinstance(y, np.ndarray) and y.ndim == 2 and y.shape[1] > 1:
@@ -72,25 +76,30 @@ class TabularData(Data):
                 y = pd_type(y, name="target")
         return y
 
-    def _validate_y(self):
-        if self.X is not None:
-            """Basic validation for y"""
-            if len(self.X) != len(self.y):
-                raise ValidationError(
-                    "X and y are not the same length. "
-                    + f"X Length: {len(self.X)}, y Length: {len(self.y)}"
-                )
-
-    def _validate_processed_y(self):
-        if isinstance(self.X, (pd.Series, pd.DataFrame)) and not self.X.index.equals(
-            self.y.index
-        ):
-            raise ValidationError("X and y must have the same index")
-
     def _validate_X(self):
-        """Basic validation for X"""
-        # Validate that the data column names are unique
+        try:
+            pd.DataFrame(self.X)
+        except:
+            raise ValidationError("X must be able to be coerced into a dataframe")
+
+    def _validate_y(self):
+        """Validation of Y inputs"""
+        if self.X is not None and (len(self.X) != len(self.y)):
+            raise ValidationError(
+                "X and y are not the same length. "
+                + f"X Length: {len(self.X)}, y Length: {len(self.y)}"
+            )
+
+    def _validate_processed_X(self):
+        """Validate processed X"""
         if len(self.X.columns) != len(set(self.X.columns)):
             raise ValidationError("X contains duplicate column names")
         if len(self.X.index) != len(set(self.X.index)):
             raise ValidationError("X's index cannot contain duplicates")
+
+    def _validate_processed_y(self):
+        """Validate processed Y"""
+        if isinstance(self.X, (pd.Series, pd.DataFrame)) and not self.X.index.equals(
+            self.y.index
+        ):
+            raise ValidationError("X and y must have the same index")
