17bfbc46e3ff2f67a97e95dfe1fc3fac483381b2
fabrizio-credo <fabrizio@credo.ai>
2022-12-06 11:39:39 -0800

    Refactored metric dependent regression tests

--- tests/test_regression.py
+++ tests/test_regression.py
@@ -14,6 +14,7 @@ from credoai.evaluators import (
     ModelEquity,
     ModelFairness,
     Performance,
+    Security,
 )
 
 
@@ -21,15 +22,21 @@ TEST_METRICS = [["r2_score"]]
 TEST_METRICS_IDS = ["regression_metric"]
 
 
-# class TestModelFairness(Base_Evaluator_Test):
-#     @pytest.mark.parametrize("metrics", TEST_METRICS, ids=TEST_METRICS_IDS)
-#     def test_full_run(self, metrics):
-#         evaluator = ModelFairness(metrics)
-#         self.pipeline.add(evaluator)
-#         self.pipeline.run()
-#         assert len(self.pipeline.pipeline) == 4
-#         assert self.pipeline.get_results()
-#         self.pipeline.pipeline = {}
+@pytest.mark.parametrize(
+    "evaluator",
+    [ModelFairness, Performance],
+    ids=["Model Fairness", "Performance"],
+)
+@pytest.mark.parametrize("metrics", TEST_METRICS, ids=TEST_METRICS_IDS)
+def test_modelfairness_performance(init_lens_regression, metrics, evaluator):
+    lens, temp_file, gov = init_lens_regression
+    eval = evaluator(metrics)
+    lens.add(eval)
+    lens.run()
+    pytest.assume(lens.get_results())
+    pytest.assume(lens.get_evidence())
+    pytest.assume(lens.send_to_governance())
+    pytest.assume(not gov._file_export(temp_file))
 
 
 @pytest.mark.parametrize(
@@ -48,14 +55,3 @@ def test_generic_evaluator(init_lens_regression, evaluator):
     pytest.assume(lens.get_evidence())
     pytest.assume(lens.send_to_governance())
     pytest.assume(not gov._file_export(temp_file))
-
-
-# class TestPerformance(Base_Evaluator_Test):
-#     @pytest.mark.parametrize("metrics", TEST_METRICS, ids=TEST_METRICS_IDS)
-#     def test_full_run(self, metrics):
-#         evaluator = Performance(metrics)
-#         self.pipeline.add(evaluator)
-#         self.pipeline.run()
-#         assert len(self.pipeline.pipeline) == 1
-#         assert self.pipeline.get_results()
-#         self.pipeline.pipeline = {}
