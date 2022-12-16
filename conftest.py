20cf32a3b6f43e7b53463fd6fac5ca214755d172
fabrizio-credo <fabrizio@credo.ai>
2022-12-15 16:56:02 -0800

    Testing different solution

---
+++ conftest.py
@@ -0,0 +1,10 @@
+"""
+Handler of local plugins.
+"""
+
+pytest_plugins = [
+    "tests.fixtures.datasets",
+    "tests.fixtures.frozen_tests",
+    "tests.fixtures.lens_artifacts",
+    "tests.fixtures.lens_inits",
+]
