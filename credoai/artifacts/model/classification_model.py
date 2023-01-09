a3abe9858e30c4286e6b52b8389d3f67945de0ba
Eli Sherman <114082449+esherman-credo@users.noreply.github.com>
2023-01-08 23:50:57 -0500

    Update credoai/artifacts/model/classification_model.py
    Co-authored-by: Ian <85892367+IanAtCredo@users.noreply.github.com>

--- credoai/artifacts/model/classification_model.py
+++ credoai/artifacts/model/classification_model.py
@@ -100,6 +100,7 @@ class ClassificationModel(Model):
 
         elif self.model_info["framework"] == "credoai":
             # Functionality for DummyClassifier
+            self.model_like = getattr(self.model_like, "model_like", None)
             if self.model_like.model_like:
                 # If the dummy model has a model_like specified, reassign
                 # the classifier's model_like attribute to match the dummy's
