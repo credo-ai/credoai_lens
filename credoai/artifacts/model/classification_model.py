c3820110c6d767bbd1b575ecce236cb092e8adac
esherman-credo <esherman@credo.ai>
2023-03-27 15:22:54 -0400

    Fix check for non-numeric series

--- credoai/artifacts/model/classification_model.py
+++ credoai/artifacts/model/classification_model.py
@@ -210,7 +210,7 @@ class DummyClassifier:
             if isinstance(array, pd.Series):
                 if not len(array):
                     raise Exception("Provided series for y_pred or y_prob is empty")
-                if array.isinf().any():
+                if np.issubdtype(array, np.number) and np.isinf(array).any():
                     raise Exception(
                         "Provided series for y_pred or y_prob contains infinite values"
                     )
